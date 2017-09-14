a69de79b0fdaeafb3eace3e8da9c69248eff7929
deadlybutter <thedeadlybutter@gmail.com>
2017-09-14 10:58:59 -0400

    Adding babel eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
+  // Use Babel parser when running linter so
+  // we can use newer language features.
+  parser: 'babel-eslint',
+
+  // Extend our DoSomething.org code style.
+  // https://github.com/dosomething/eslint-config
   extends: '@dosomething/eslint-config',
 };
