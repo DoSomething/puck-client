7cb1df77d11a7c362d7f15aeb7d731a71d671f0b
deadlybutter <thedeadlybutter@gmail.com>
2017-11-03 14:00:14 -0400

    adding logic to fetch the cookie id

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const configure = require('@dosomething/webpack-config');
 const path = require('path');
 
 module.exports = configure({
-  entry: {
-    app: './lib/index.js'
-  },
+  entry: [
+    'whatwg-fetch',
+    'babel-polyfill',
+    './lib/index.js',
+  ],
   module: {
     loaders: [
       { enforce: 'pre', test: /\.js$/, use: 'eslint-loader', exclude: /node_modules/ },
